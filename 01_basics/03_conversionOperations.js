6c6b3cc68e222c4aa1ba102b2c4f188cc4974d95
Shashwat Kumar <144956225+shashwatgithubworld@users.noreply.github.com>
2023-11-08 08:14:23 +0000

    conversions of dataType

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+
+// --Number conversion-- 
+
+let score = "Delhi"
+
+let valueInNumber = Number(score);
+
+// "44" => 44
+// "44abc" => NaN
+// true => 1; false => 0;
+
+//  --Boolean conversion--
+
+let isLoggedIn = 5
+
+let isBooleanLoggedIn = Boolean(isLoggedIn);
+
+// 1 => true; 0 => false 
+// "" => false
+// "Delhi" => true
+
+//   --string conversion--
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
