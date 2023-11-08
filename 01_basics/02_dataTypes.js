eb842d556a15c7859fba04f6f94dc3d82761b4f1
Shashwat Kumar <144956225+shashwatgithubworld@users.noreply.github.com>
2023-11-08 06:13:36 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+//  DataTypes in JavaScript
+//1. Primitive DataTypes
+//2. Non-premitive DataTypes
+
+/*
+          --Premitive dataType--
+    1. number = 123 =>range- 2 to the power 52
+    2. bigint
+    3. string = ""
+    4. boolean => true/false
+    5. null => standalone value
+    6. undefined =>
+    7. symbol => unique   
+*/
+
+/*
+        --Non-premitive dataType--
+     Object Datatype
+       The object data type can contain:
+         1. An object
+         2. An array
+         3. A date
+*/
+
+console.log(typeof null); // object
+console.log(typeof undefined);  // undefined
