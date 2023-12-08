66428430b9105541fc26777130cf96f4cdb3e384
Shashwat Kumar <144956225+shashwatgithubworld@users.noreply.github.com>
2023-12-09 04:55:34 +0530

    Update 05_strings.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,10 +1,13 @@
 const name = "shashwat"
 const repoCount = 50
 
-// console.log(name + repoCount + " Value");
-//output = shashwat50 value
+// console.log(name + repoCount + " Value"); => not recommended
+//output => shashwat50 value
+
+// In modern JS we use backticks(``) for string interpolation. basically we create a place holder 
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+//output => Hello my name is shashwat and my repo count is 50
 
 const gameName = new String('shashwat-singh')
 
