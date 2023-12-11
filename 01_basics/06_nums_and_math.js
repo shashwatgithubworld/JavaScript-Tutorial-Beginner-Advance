d372acce16a4384287a646ef4c017a299ba5f71c
Shashwat Kumar <144956225+shashwatgithubworld@users.noreply.github.com>
2023-12-11 17:55:50 +0000

    Added some comments

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -3,7 +3,6 @@ const score = 400
 
 const balance = new Number(100)
 // console.log(balance);
-
 // console.log(balance.toString().length);
 // console.log(balance.toFixed(1));
 
@@ -12,19 +11,35 @@ const otherNumber = 123.8966
 // console.log(otherNumber.toPrecision(4));
 
 const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString()); 
+// output => 1,000,000
+
+// console.log(hundreds.toLocaleString('en-IN')); (Now, it will show india standard values)
+// output => 10,00,000
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
 // console.log(Math);
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.6));
-// console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.9));
-// console.log(Math.min(4, 3, 6, 8));
-// console.log(Math.max(4, 3, 6, 8));
+// console.log(Math.abs(-4)); => returns absolute values, work with substraction only.
+// output => 4
 
-console.log(Math.random());
+// console.log(Math.round(4.6)); => returns roundof values 
+// output => 5
+
+// console.log(Math.ceil(4.2)); => always returns the highest value
+// Output => 5
+
+// console.log(Math.floor(4.9)); => always returns the lowest value
+// output => 4
+
+// console.log(Math.min(4, 3, 6, 8)); => to find the lowest value in given array
+// Output => 3
+
+// console.log(Math.max(4, 3, 6, 8)); => to find the highest value in the given array
+// Output => 8
+
+// console.log(Math.random()); => gives a randam number b/w 0-1
+
 console.log((Math.random()*10) + 1);
 console.log(Math.floor(Math.random()*10) + 1);
 
