04aa2e855cfc670becd26e581962d9b5bf2dc1c4
Shashwat Kumar <144956225+shashwatgithubworld@users.noreply.github.com>
2023-12-13 20:23:17 +0000

    Added some comments for better understanding

--- 02_basics/01_arrays1.js
+++ 02_basics/01_arrays1.js
@@ -24,10 +24,11 @@ const myArr = [0, 1, 2, 3, 4, 5]
 // myArr.shift() => remove element from start
 // output => [1, 2, 3, 4, 5]
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+// console.log(myArr.includes(9)); => to check that element is included in the given array (returns - true or false)
 
-// const newArr = myArr.join()
+// console.log(myArr.indexOf(3)); => to check elemnt is available on which index.
+
+// const newArr = myArr.join() => it binds the same array and convert to string
 
 // console.log(myArr);
 // console.log(typeof newArr)
@@ -37,13 +38,21 @@ const myArr = [0, 1, 2, 3, 4, 5]
 // slice, splice
 
 console.log("A ", myArr);
+// Output => A  [ 0, 1, 2, 3, 4, 5 ]
 
-const myn1 = myArr.slice(1, 3)
+const myn1 = myArr.slice(1, 3)  /*The slice() method of Array instances returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included)*/
+// console.log(myn1);
+// output => [ 1, 2 ]
 
-console.log(myn1);
+
 console.log("B ", myArr);
+// output => B  [ 0, 1, 2, 3, 4, 5 ]
 
 
-const myn2 = myArr.splice(1, 3)
+const myn2 = myArr.splice(1, 3) /*The splice() method of Array instances changes the contents of an array by removing  existing elements. */
+// console.log(myn2);
+// output => [ 1, 2, 3 ]
+
+
 console.log("C ", myArr);
-console.log(myn2);
+// output => C  [ 0, 4, 5 ]
