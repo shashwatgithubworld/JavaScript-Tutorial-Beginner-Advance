39d3d295ca03fe9f265316eeae61113cc1dcf49f
Shashwat Kumar <144956225+shashwatgithubworld@users.noreply.github.com>
2023-12-16 21:51:04 +0000

    Nested scopes and hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -10,3 +10,49 @@ if (true) {
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+// *******  Nested Scopes *****\\
+
+function one(){
+    const username = "Shashwat"
+
+    function two(){
+        const website = "Google"
+        console.log(username);
+    }
+    // console.log(website);
+
+    // two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "Shashwat"
+    if (username === "Shashwat") {
+        const website = " Singh"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ Hoisting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+// output => 6
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+// output => error
