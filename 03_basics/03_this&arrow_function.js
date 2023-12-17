00d47a4cb55fc641c2f233cc7b9fa1918f81e5c3
Shashwat Kumar <144956225+shashwatgithubworld@users.noreply.github.com>
2023-12-17 02:26:57 +0000

    This keyword

---
+++ 03_basics/03_this&arrow_function.js
@@ -0,0 +1,16 @@
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
