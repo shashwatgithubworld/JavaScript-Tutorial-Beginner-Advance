ba7aa2b89e8f28f5d33b0fd6465cbc13f67419d8
Shashwat Kumar <144956225+shashwatgithubworld@users.noreply.github.com>
2023-12-17 03:01:39 +0000

    arrow function

--- 03_basics/03_this&arrow_function.js
+++ 03_basics/03_this&arrow_function.js
@@ -1,5 +1,5 @@
 const user = {
-    username: "hitesh",
+    username: "shashwat",
     price: 999,
 
     welcomeMessage: function() {
@@ -14,3 +14,47 @@ const user = {
 // user.welcomeMessage()
 
 // console.log(this);
+
+// function vodka(){
+//     let username = "shashwat"
+//     console.log(this.username);
+// }
+
+// vodka()
+
+
+// const wisky = function () {
+//     let username = "shashwat"
+//     console.log(this.username);
+// }
+
+/************** arrow functions********** */
+
+const daru =  () => {     // syntx for arrow function
+    let username = "shashwat"
+    console.log(this);
+}
+// daru()
+
+// example =>
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2 // another way of declare arrow function
+// console.log(addTwo(3, 4))
+// output => 7
+
+
+// const addTwo = (num1, num2) => ( num1 + num2 ) //another way of declare arrow function
+// console.log(addTwo())
+// output => 7
+
+
+const addTwo = (num1, num2) => ({username: "shashwat"}) // to return object
+
+console.log(addTwo())
+// output => { username: 'shashwat' }
+
+
+
