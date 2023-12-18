30fc3b4a0e19b5bfc69687eed615e5841983a6ac
Shashwat Kumar <144956225+shashwatgithubworld@users.noreply.github.com>
2023-12-18 20:24:56 +0000

    IF ELSE

--- 05_control_flow_logics/0ne.js
+++ 05_control_flow_logics/0ne.js
@@ -1,8 +1,8 @@
 // IF 
 
-if (true) {
-    console.log("shashwat");
-}
+// if (true) {
+//     console.log("shashwat");
+// }
 
 //*****  Comparison Operators ***** \\
 /*
@@ -22,5 +22,30 @@ if (true) {
   &&	and
   ||	or
   !	    not  
-
 */
+
+// **** IF ELSE ***** \\
+
+// const temprature = 32 
+
+// if (temprature < 52 ) {
+//    console.log("Temprature is less than 52.");
+// }
+// output => Temprature is less than 52
+
+// const temprature1 = 32 
+
+// if ( temprature1 === 40 ){
+//     console.log("less than 50");
+// } else {
+//          console.log("temperature is greater than 50");
+//      }
+ // output => temperature is greater than 50
+
+ // if (score > 100) {
+//     let power = "fly"
+//     console.log(`User can: ${power}`);
+// }
+
+// output => User can: fly
+
