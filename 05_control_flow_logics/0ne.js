e1eee055f4106dbe6b04a39d02cf3530e0d0d99e
Shashwat Kumar <144956225+shashwatgithubworld@users.noreply.github.com>
2023-12-18 20:39:23 +0000

    Nested condition

--- 05_control_flow_logics/0ne.js
+++ 05_control_flow_logics/0ne.js
@@ -1,4 +1,4 @@
-// IF 
+// IF
 
 // if (true) {
 //     console.log("shashwat");
@@ -17,35 +17,74 @@
   <=    less than or equal to
 */
 
-// ****** Logical Operators  ****** \\
-/*
-  &&	and
-  ||	or
-  !	    not  
-*/
+
 
 // **** IF ELSE ***** \\
 
-// const temprature = 32 
+// const temprature = 32
 
 // if (temprature < 52 ) {
 //    console.log("Temprature is less than 52.");
 // }
 // output => Temprature is less than 52
 
-// const temprature1 = 32 
+// const temprature1 = 32
 
 // if ( temprature1 === 40 ){
 //     console.log("less than 50");
 // } else {
 //          console.log("temperature is greater than 50");
 //      }
- // output => temperature is greater than 50
+// output => temperature is greater than 50
 
- // if (score > 100) {
+// if (score > 100) {
 //     let power = "fly"
 //     console.log(`User can: ${power}`);
 // }
 
 // output => User can: fly
 
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2"); // Shorthand condition
+
+// ******  Nested condition ***** \\
+
+/*
+if (balance < 500) {
+  console.log("less than 500");
+
+} else if (balance < 750) {
+  console.log("less than 750");
+
+} else if (balance < 900) {
+  console.log("less than 900");
+
+} else {
+  console.log("less than 1200");
+
+}
+*/
+// output => less than 1200
+
+// ****** Logical Operators  ****** \\
+/*
+  &&	and
+  ||	or
+  !	  not  
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
+
+*/
