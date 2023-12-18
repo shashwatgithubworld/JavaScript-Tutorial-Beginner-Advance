05feb7aa897e9f7e92a0bb389455b6b1c41d8586
Shashwat Kumar <144956225+shashwatgithubworld@users.noreply.github.com>
2023-12-18 19:59:25 +0000

    Operatores

---
+++ 05_control_flow_logics/0ne.js
@@ -0,0 +1,26 @@
+// IF 
+
+if (true) {
+    console.log("shashwat");
+}
+
+//*****  Comparison Operators ***** \\
+/*
+
+  >	    greater than
+  <	    less than
+  ==    equal to
+  !=    not equal
+  ===  	equal value and equal type
+  !==	not equal value or not equal type
+  >=    greater than or equal to
+  <=    less than or equal to
+*/
+
+// ****** Logical Operators  ****** \\
+/*
+  &&	and
+  ||	or
+  !	    not  
+
+*/
