4cdf8bc2e8c134898db81b05d2ce7d362898e6be
Shashwat Kumar <144956225+shashwatgithubworld@users.noreply.github.com>
2023-12-21 05:32:50 +0000

    high order array loop

---
+++ 06_Loops/for_each_loop.js
@@ -0,0 +1,55 @@
+// for each
+
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+coding.forEach( (item, index, arr) => {
+      
+    // console.log(item, index, arr);
+})
+/* Output => 
+js 0 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+ruby 1 [ 'js', 'ruby', 'java', 'python', 'cpp' ]  
+java 2 [ 'js', 'ruby', 'java', 'python', 'cpp' ]  
+python 3 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+cpp 4 [ 'js', 'ruby', 'java', 'python', 'cpp' ] 
+*/
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    // console.log(item.languageName);
+} )
+/* Output => 
+javascript
+java
+python
+*/
+
